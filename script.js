f8251813f4fa6664736b8069a4e9ffad222d1857
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2023-10-12 00:24:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-//your code here
+// Write your code here
 
+let touristSpots = ["The Virupaksha Temple", "Victoria Memorial", "TajMahal"];
+
+function removeArticles(name) {
+  const articles = ["a", "an", "the"];
+  for (const article of articles) {
+    if (name.toLowerCase().startsWith(article + " ")) {
+      return name.slice(article.length + 1).trim();
+    }
+  }
+  return name;
+}
+
+touristSpots.sort((a, b) => (removeArticles(a) > removeArticles(b) ? 1 : -1));
+
+touristSpots.forEach(function (touristSpots) {
+  console.log(touristSpots);
+});
